e3d321deeabd8f2d86bbdaa65fa0f09ab8dc993f
Braden <bradenallred77@gmail.com>
2022-01-10 14:10:20 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Braden Allred on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
