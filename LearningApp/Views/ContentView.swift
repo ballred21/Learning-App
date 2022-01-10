0fb83a9a5d71b38ef3a26c8862d2ba539afb332f
Braden <bradenallred77@gmail.com>
2022-01-10 17:53:14 -0400

    - fix padding and alignment

--- LearningApp/Views/ContentView.swift
+++ LearningApp/Views/ContentView.swift
@@ -22,9 +22,11 @@ struct ContentView: View {
                 if model.currentModule != nil {
                 
                     ForEach(0..<model.currentModule!.content.lessons.count) { index in
+                        
+                        let lesson = model.currentModule!.content.lessons[index]
                     
                     // Lesson Card
-                    ZStack {
+                    ZStack (alignment: .leading) {
                         
                         Rectangle()
                             .foregroundColor(.white)
@@ -32,29 +34,29 @@ struct ContentView: View {
                             .shadow(radius: 5)
                             .frame(height: 66)
                         
-                        HStack {
+                        HStack (spacing: 30) {
                             
                             Text(String(index + 1))
+                                .bold()
                             
                             VStack(alignment: .leading) {
-                                Text("Variables and Constants")
-                                Text("17 minutes")
+                                Text(lesson.title)
+                                    .bold()
+                                Text(lesson.duration)
                             }
                             
-                        }
+                        }.padding()
                         
                     }
+                    .padding(.bottom, 4)
                     
                 }
                 }
             }
+            .padding()
+            .navigationTitle("Learn \(model.currentModule?.category ?? "")")
         }
         
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
